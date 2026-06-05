7fb33ac43e5db5e1a89b9c771701cd0e292a8139
kotentie <jakekoten@gmail.com>
2026-06-05 13:55:07 -0700

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,10 @@
+
+
+cal.js
+
+function layOutDay(events) {}
+This function will be invoked from the console for testing purposes. If it cannot be invoked, the submission will be rejected.
+ 
+In your submission, please implement the calendar with the following input:
+ 
+[ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
